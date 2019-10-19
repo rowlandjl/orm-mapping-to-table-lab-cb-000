e67b656383b7126ca265a1ecacc4b8b3f99633e8
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-19 17:34:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -20,14 +20,6 @@ class Student
         )
     SQL
     
-    sql = <<-SQL
-      CREATE TABLE IF NOT EXISTS students (
-        id INTEGER PRIMARY KEY,
-        name TEXT,
-        grade TEXT
-        )
-    SQL
-    
     DB[:conn].execute(sql)
   end 
   
