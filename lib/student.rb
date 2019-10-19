3bbb64a3afb5743de715629d76ce77b74263faad
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-19 17:40:29 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -39,7 +39,7 @@ class Student
     
     DB[:conn].execute(sql, self.name, self.grade)
     
-    @id = DB[:conn].execute("SELECT last_insert_")
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")
   end 
   
   def self.create 
