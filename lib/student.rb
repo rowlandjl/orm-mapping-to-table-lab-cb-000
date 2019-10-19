ace815a4448e22b0d6ccbb621ec29e2140596e3c
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-19 17:24:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,7 +13,9 @@ class Student
   
   def self.create_table 
     sql = <<-SQL
-      CREATE TABLE IF NOT EXISTS students 
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        )
   end 
   
   def self.drop_table 
