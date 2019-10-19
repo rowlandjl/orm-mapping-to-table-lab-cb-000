b7a80911eb119da04e9639afc935b5d5aa761dd0
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-19 17:27:45 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,12 +13,12 @@ class Student
   
   def self.create_table 
     sql = <<-SQL
-      # # CREATE TABLE IF NOT EXISTS students (
-      # #   id INTEGER PRIMARY KEY,
-      # #   name TEXT,
-      # #   grade TEXT
-      # #   )
-      #   SQL 
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade TEXT
+        )
+        SQL 
     DB[:conn].execute(sql)
   end 
   
